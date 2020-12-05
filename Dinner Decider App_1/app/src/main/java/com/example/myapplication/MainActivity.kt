6fc2c49f9bd17648ab624c1c9abddf8b2c41479f
Sabrina1 <you@example.com>
2020-12-05 18:16:56 +0000

    completed with some trics

--- Dinner Decider App_1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Dinner Decider App_1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // selectedFoodTxt.text = "Burger King"
+        // selectedFoodTxt.text = "Burger King"
         decideBtn.setOnClickListener {
             //println("You clicked me!") we know this is working as this message is appeared the number of times we click the button
             val random = Random()
@@ -28,5 +29,27 @@ class MainActivity : AppCompatActivity() {
             addFoodTxt.text.clear()
             println(foodList)
         }
+
     }
 }
+
+
+//Tricks
+//1 Comment cmd + fwd /
+//2 Command+shift+slash--multi line
+//reformat Code
+//3-Reformat follow below
+//Mac-Option+L
+//Win-Ctrl+Alt+L
+//4
+//HIGLIGHT
+//SHIFT+CTRL+<-->
+//5 S+C+UPARROW OR dOWN ONE
+//6 DUPLICATE LINE->CTRL+D
+
+//7
+// c+S+A --> SERACH ANYTHING
+// 8 - RIGHTCLICK SPLIT
+//9 - DOUBLE CLICK ON SHIFT--TYPE FIND ME-- TO DO SEARCH EVERYWHERE
+//10-CTRL+ O METHODS TO OVERRIDE
+//11 con restore what copy earlier c+s+v
