ece826b4d08126446f3179121e1b31c5e75b3bbd
Sabrina1 <you@example.com>
2020-12-05 16:29:31 +0000

    made the app button to work

--- Dinner Decider App_1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Dinner Decider App_1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    //2 after testing the button create a list
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Barros Pizza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+       // selectedFoodTxt.text = "Burger King"
+        decideBtn.setOnClickListener {
+            //println("You clicked me!") we know this is working as this message is appeared the number of times we click the button
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
     }
 }
